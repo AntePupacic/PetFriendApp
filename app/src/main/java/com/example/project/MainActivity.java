b8846aca82a80bb679d53711b8baf321fdc35839
AntePupacic <apupac00@gmail.com>
2021-11-20 22:26:21 +0100

    added some comment

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         dogBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                //Open new activity
             }
         });
     }
