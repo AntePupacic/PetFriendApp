f1bfdfecb5c3fa12f4516add2492c0acc4115b10
AntePupacic <apupac00@gmail.com>
2021-12-10 09:50:17 +0100

    Making layout for adding into the DB

--- app/src/main/java/com/example/project/DogList.java
+++ app/src/main/java/com/example/project/DogList.java
@@ -6,12 +6,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class DogList extends AppCompatActivity {
 
     private RecyclerView dogRecycleView;
     private PetAdapter.RecycleViewClickListener listener;
+    private Button addBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +21,16 @@ public class DogList extends AppCompatActivity {
         setContentView(R.layout.activity_dog_list);
 
         DataStorage.fillData();
+        recycleViewClickListener();
+        addBtn = (Button) findViewById(R.id.addBtn);
+        dogRecycleView = findViewById(R.id.dogRecycleView);
+        PetAdapter petAdapter = new PetAdapter(getApplicationContext(), listener);
+        dogRecycleView.setAdapter(petAdapter);
+
+    }
 
 
+    private void recycleViewClickListener(){
         listener = new PetAdapter.RecycleViewClickListener() {
             @Override
             public void onClick(View v, int position) {
@@ -29,10 +39,14 @@ public class DogList extends AppCompatActivity {
                 startActivity(intent);
             }
         };
-        dogRecycleView = findViewById(R.id.dogRecycleView);
-        PetAdapter petAdapter = new PetAdapter(getApplicationContext(), listener);
-        dogRecycleView.setAdapter(petAdapter);
+    }
 
-
+    private void addPet(){
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DogList.this, PetInfo.class);
+            }
+        });
     }
 }
