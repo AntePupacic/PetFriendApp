fc69c4b885cb30ba8cb01404803c52db3c646ef8
AntePupacic <apupac00@gmail.com>
2021-12-20 21:04:48 +0100

    Adding some comments

--- app/src/main/java/com/example/project/AddPet.java
+++ app/src/main/java/com/example/project/AddPet.java
@@ -15,6 +15,8 @@ import android.widget.Toast;
 
 public class AddPet extends AppCompatActivity {
 
+
+    //Bakota
     EditText editTxtPetName, editTxtPetDescription, editTxtLocation, editTxtAge;
     Button btnAddPet, btnAddImg;
     String petName, petDescription, petLocation, petAge;
